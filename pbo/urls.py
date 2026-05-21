6c2a15e10f92ef6ce76ee2ea1450204fc6996351
Rahmat Hidayat <147997416+Rahmatbaaka@users.noreply.github.com>
2026-05-21 10:47:22 +0700

    .

--- pbo/urls.py
+++ pbo/urls.py
@@ -2,10 +2,9 @@
 
 from django.contrib import admin
 from django.urls import path
-from dashboard.views import dashboard_view  # Mengimpor fungsi dari views.py
+from dashboard.views import dashboard_view 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # Baris di bawah ini yang akan memunculkan dashboard di halaman     utama (root URL)     
+    path('admin/', admin.site.urls),    
     path('', dashboard_view, name='dashboard'), 
 ]
